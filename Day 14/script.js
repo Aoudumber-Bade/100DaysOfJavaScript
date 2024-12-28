978298ebc174d4fdbec8151fdfbe66e967e46004
Aoudumber <aoudumberbade@gmail.com>
2024-12-29 01:04:23 +0530

    Day 14

---
+++ Day 14/script.js
@@ -0,0 +1,16 @@
+
+
+// ? Write a function called isPowerOfTwo() that takes an integer num as input and return true if num is a power of two, and false otherwise.
+
+const isPowerOfTwo = (num) => {
+    let op = false; 
+    for(let i = 0; i<num; i++) {
+        if(2**i === num) {
+            op = true;
+        }
+    }
+
+    return op;
+}
+
+console.log(isPowerOfTwo(16)); // true
